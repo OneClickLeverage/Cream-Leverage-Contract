8f599ceb0c911568099d9f75dbe7cd6ba150f984
SeungJu Lee <sjtogo@gmail.com>
2021-09-05 16:54:26 +0400

    Add tab action

--- ui/src/LeveragePopUp.tsx
+++ ui/src/LeveragePopUp.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { DeleveragePopUpBody } from './components/DeleveragePopUpBody';
 import LeveragePopUpBody from './components/LeveragePopUpBody';
 import "./LeveragePopUp.css";
 import { TokenID } from './types/TokenID';
@@ -7,22 +8,45 @@ interface Props {
   debtToken: TokenID,
 }
 
+enum TabIndex {
+  Leverage = 0,
+  Deleverage = 1
+}
+
 export function LeveragePopUp(props: Props) {
+  const [tabIndex, setTabIndex] = useState<TabIndex>(TabIndex.Deleverage)
+
+  function onTabClick(i: TabIndex) {
+    setTabIndex(i)
+  }
+
   return (
     <div className="leverage-outer">
       <div className="leverage-inner">
         <div style={{ display: "flex" }}>
-          <div className="borrow-tab borrow-tab--active">
+          <div
+            onClick={() => onTabClick(TabIndex.Leverage)}
+            className={`borrow-tab ${tabIndex === TabIndex.Leverage ? 'borrow-tab--active': 'borrow-tab--inactive'}`}
+          >
             Leverage
           </div>
-          <div className="borrow-tab borrow-tab--inactive">
+          <div
+            onClick={() => onTabClick(TabIndex.Deleverage)}
+            className={`borrow-tab ${tabIndex === TabIndex.Deleverage ? 'borrow-tab--active': 'borrow-tab--inactive'}`}
+          >
             Deleverage
           </div>
         </div>
-        <LeveragePopUpBody
-          collateralToken={props.collateralToken}
-          debtToken={props.debtToken}
-        />
+        { tabIndex === TabIndex.Leverage &&
+          <LeveragePopUpBody
+            collateralToken={props.collateralToken}
+            debtToken={props.debtToken}
+          />
+        }
+        { tabIndex === TabIndex.Deleverage &&
+          <DeleveragePopUpBody
+          />
+        }
       </div>
     </div>
   )
